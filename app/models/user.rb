c1b5840113a941110da28b9f291d49ed71c44803
Ronald Igar <ronaldigar@yahoo.com>
2020-08-05 15:03:16 +0800

    service done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   SPECIALITIES = %W(bohemian minimalist modern industrial)
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :services, dependent: :destroy
   has_many :bookings, dependent: :destroy
   validates :name, uniqueness: true, presence: true
