51b6afc64271da1cd2d026549236387b7bf36306
Mickael Taillepierre <mick0987@hotmail.ch>
2020-08-05 11:29:24 +0200

    front

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -8,18 +8,38 @@ class ServicesController < ApplicationController
     @service = Service.new
   end
 
-  def profile
-  end
-
   def create
+    # @service = service.new(name: params[:name], address: params[:address], rating: params[:rating])
+    # new record with values from form
     @service = Service.new(service_params)
-    # @user = User.find(params[:user_id])
-    @service.user = current_user
-    @service.save
+
+    if @service.save
+        redirect_to service_path(@service)
+    else
+        render :new
+    end
+    end
+
+    def edit
+    # form with service info filled out
+    @service = service.find(params[:id])
+    end
+
+
+    def update
+    @service.update(service_params)
+    # redirect to the show page
+    redirect_to @service
+    end
+
+
+    def destroy
+    @service.destroy
     redirect_to services_path
-  end
+    end
 
   private
+  
   def service_params
     params.require(:service).permit(:description, :rate, :speciality, :project_type, :user)
   end
