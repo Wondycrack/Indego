5a85d90e4284237fb15cdbe92a4614753cb00a27
Mickael Taillepierre <mick0987@hotmail.ch>
2020-08-07 09:00:21 +0200

    search

---
+++ app/javascript/components/carrousel.js
@@ -0,0 +1,8 @@
+const initCarousel = () => {
+$('#carousel .item img').each(function() {
+    var imgSrc = $(this).attr('src');
+    $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+    $(this).remove();
+  });
+}
+export default initCarousel;
